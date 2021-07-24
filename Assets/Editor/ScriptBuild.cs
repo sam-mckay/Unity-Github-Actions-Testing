987c7e0e4cd7c58078980b5358ef3d6646e95bdf
Sam <sammckay@btinternet.com>
2021-07-24 14:30:46 +0100

    Remove erroneous parameter
    Fix script build

--- Assets/Editor/ScriptBuild.cs
+++ Assets/Editor/ScriptBuild.cs
@@ -8,15 +8,15 @@ public static class ScriptBuild
     public static void Build()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] { "Assets/scene.unity" };
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
         buildPlayerOptions.locationPathName = "scriptBuilds";
-        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
 
         // use these options for the first build
         buildPlayerOptions.options = BuildOptions.Development;
 
         // use these options for building scripts
-        // buildPlayerOptions.options = BuildOptions.BuildScriptsOnly | BuildOptions.Development;
+         buildPlayerOptions.options = BuildOptions.BuildScriptsOnly | BuildOptions.Development;
 
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
