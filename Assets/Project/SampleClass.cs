c404ad74bc367b023b295f5204e88c254c899b5b
Sam <sammckay@btinternet.com>
2021-08-22 10:39:24 +0100

    Fix linter warnings

--- Assets/Project/SampleClass.cs
+++ Assets/Project/SampleClass.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class SampleClass 
+public static class SampleClass
 {
     //tst csharp lint
-    public static void Method1()    {
+    public static void Method1()
+    {
 
     }
 }
